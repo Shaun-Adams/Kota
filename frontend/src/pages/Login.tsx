91197e66ee9dd789804ec47f33173888f5b9fc23
Shaun Adams <shaunsiadams@gmail.com>
2024-03-08 17:12:37 +0200

    update

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -27,7 +27,7 @@ const Login: React.FC = () => {
   };
 
   return (
-  <div className="flex flex-col items-center justify-center h-screen border-2 border-gray-300 p-4 rounded">
+  <div className="flex flex-col items-center justify-center h-screen gap-4 border-2 border-gray-300 p-4 rounded">
     <h2 className="text-3xl font-bold mb-4">Log In</h2>
     <div className="flex flex-col gap-4 ">
       <Input
@@ -40,7 +40,10 @@ const Login: React.FC = () => {
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-      <Button onClick={handleLogin} className="bg-gradient-to-r from-yellow-300 to-yellow-500 backdrop-filter backdrop-blur-md text-black px-4 py-2 rounded-lg">Log in</Button>
+      <Button onClick={handleLogin} color='danger'>Log in</Button>
+      <Button className="bg-gradient-to-r from-yellow-300 to-yellow-500 backdrop-filter backdrop-blur-md text-black px-4 py-2 rounded-lg" onClick={() => Router.push('/register')}>
+            Dont have an account? Register
+        </Button>
     </div>
   </div>
 );
--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -25,7 +25,7 @@ const Register: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen border-2 border-gray-300 p-4 rounded">
+    <div className="flex flex-col items-center justify-center h-screen border-2 gap-4 border-gray-300 p-4 rounded">
       <h2 className="text-3xl font-bold mb-4">Register</h2>
       <div className="flex flex-col gap-4">
         <Input
@@ -38,8 +38,8 @@ const Register: React.FC = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <Button onClick={handleRegister} className="bg-gradient-to-r from-yellow-300 to-yellow-500 backdrop-filter backdrop-blur-md text-black px-4 py-2 rounded-lg">Register</Button>
-        <Button  color='danger' onClick={() => Router.push('/login')}>
+        <Button color='danger' onClick={handleRegister}>Register</Button>
+        <Button className="bg-gradient-to-r from-yellow-300 to-yellow-500 backdrop-filter backdrop-blur-md text-black px-4 py-2 rounded-lg"  onClick={() => Router.push('/login')}>
             Already have an account? Log In
         </Button>
       </div>
