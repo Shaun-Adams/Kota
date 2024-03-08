5d257d40204f2513b56a19069d019ae48a32261f
Shaun Adams <shaunsiadams@gmail.com>
2024-03-08 17:08:54 +0200

    update

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -27,23 +27,23 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h2 className="text-3xl font-bold mb-4">Log In</h2>
-      <div className="flex flex-col gap-4 border-2 border-gray-300 p-4 rounded">
-        <Input
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <Input
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button onClick={handleLogin} className="bg-gradient-to-r from-yellow-300 to-yellow-500 backdrop-filter backdrop-blur-md text-black px-4 py-2 rounded-lg">Log in</Button>
-      </div>
+  <div className="flex flex-col items-center justify-center h-screen border-2 border-gray-300 p-4 rounded">
+    <h2 className="text-3xl font-bold mb-4">Log In</h2>
+    <div className="flex flex-col gap-4 ">
+      <Input
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <Input
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button onClick={handleLogin} className="bg-gradient-to-r from-yellow-300 to-yellow-500 backdrop-filter backdrop-blur-md text-black px-4 py-2 rounded-lg">Log in</Button>
     </div>
-  );
+  </div>
+);
 };
 
 export default Login;
--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -18,16 +18,14 @@ const Register: React.FC = () => {
     });
 
     if (response.ok) {
-      // const data = await response.json();
-      // localStorage.setItem('token', data.token); // Store the token
-      Router.push('/login'); // Redirect to the homepage
+      Router.push('/login'); 
     } else {
-      alert('Failed to log in');
+      alert('Failed to register');
     }
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen border-2 border-gray-300 p-4 rounded">
       <h2 className="text-3xl font-bold mb-4">Register</h2>
       <div className="flex flex-col gap-4">
         <Input
@@ -41,8 +39,8 @@ const Register: React.FC = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
         <Button onClick={handleRegister} className="bg-gradient-to-r from-yellow-300 to-yellow-500 backdrop-filter backdrop-blur-md text-black px-4 py-2 rounded-lg">Register</Button>
-        <Button  color='danger' onClick={() => Router.push('/login')} >
-            Already have an account? Log in
+        <Button  color='danger' onClick={() => Router.push('/login')}>
+            Already have an account? Log In
         </Button>
       </div>
     </div>
